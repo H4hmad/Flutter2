815f5140fea6f44f9c72ea178d1e6f77f3091151
Learning-Pythong <lamostlaaa@gmail.com>
2024-03-04 21:06:06 +1100

    Adding opacity to widgets

--- adv_basics/lib/gradient_container.dart
+++ adv_basics/lib/gradient_container.dart
@@ -18,7 +18,8 @@ class GradientContainer extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset('assets/images/quiz-logo.png', width: 300),
+              Image.asset('assets/images/quiz-logo.png',
+                  color: const Color.fromARGB(143, 255, 255, 255), width: 300),
               const SizedBox(height: 60),
               const Text('Learn Flutter the fun Way!!!',
                   style: TextStyle(
