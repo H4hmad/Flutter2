10a20a6b0772e8851373d8286f5ad3ed0aae0adb
Learning-Pythong <lamostlaaa@gmail.com>
2024-03-06 23:57:35 +1100

    Preparing for adding in the questions screen and the quiz questions.

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:adv_basics/gradient_container.dart';
+import 'package:adv_basics/quiz.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-          body: GradientContainer([
-        Color.fromARGB(255, 65, 0, 177),
-        Color.fromARGB(255, 101, 64, 202)
-      ])),
-    ),
-  );
+  runApp(const Quiz());
 }
