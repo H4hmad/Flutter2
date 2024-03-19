b661623452473e1c4e3cbedb2d22d89bc04e41fc
Learning-Pythong <lamostlaaa@gmail.com>
2024-03-20 01:47:28 +1100

    Made a bunch of tweaks and changes. APP COMPLETE

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:adv_basics/start_screen.dart';
 import 'package:adv_basics/questions_screen.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/results_screen.dart';
-
-const startAlignment = Alignment.topCenter;
-const endAlignment = Alignment.bottomCenter;
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -18,16 +16,17 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   List<String> selectedAnswers = [];
-  var activeScreen = 'start-secrrn';
+  var activeScreen = 'start-screen';
+
   void switchScreen() {
     setState(() {
-      selectedAnswers = [];
       activeScreen = 'questions-screen';
     });
   }
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
     if (selectedAnswers.length == questions.length) {
       setState(() {
         activeScreen = 'results-screen';
@@ -35,26 +34,42 @@ class _QuizState extends State<Quiz> {
     }
   }
 
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = ResultsScreen();
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
     }
 
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 65, 0, 177),
-              Color.fromARGB(255, 101, 64, 202)
-            ], begin: startAlignment, end: endAlignment),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
           child: screenWidget,
         ),
